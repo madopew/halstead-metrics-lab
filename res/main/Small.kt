ffbb4b1281c9727e2da1dc0ce521458f72533309
Bakyt Madi <bakyt.madi.work@gmail.com>
2020-10-01 21:37:31 +0300

    Add big and small exmaples

---
+++ res/main/Small.kt
@@ -0,0 +1,19 @@
+fun main() {
+    //This program outputs Fibonacci numbers
+    var l = 1;
+    var r = 1;
+    for(i in 3..50) {
+        val tmp = l
+        l += r
+        r = temp
+    }
+    
+    if(l > 1000)
+        println("It's big!");
+    else
+        println(l)
+
+    /*
+    return a+b    
+     */
+}
